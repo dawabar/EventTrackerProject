85d344c3f3ab2cb238e470d725f2d64f2f03b370
Matt Elmore <84700909+dawabar@users.noreply.github.com>
2022-06-13 01:04:51 -0400

    All five objectives met for weekend homework.  CRUD operations work and data aggregation shows total number of chores in the database. For some reason, the page does not refresh with the loadChoresList function call. Talked this out with classmates, could not resolve issue.

--- CHOREX/src/main/resources/static/js/script.js
+++ CHOREX/src/main/resources/static/js/script.js
@@ -8,31 +8,38 @@ function init() {
 	console.log('In init');
 	loadChoresList();
 	document.updateChoreForm.updateChoreButton.addEventListener('click', updateChore);
+	document.newChoreForm.newChoreButton.addEventListener('click', addChore);
 }
 
-function loadChoresList() {
+const loadChoresList = function() {
+	console.log('inside loadChoresList');
 	let xhr = new XMLHttpRequest();
-	xhr.open('GET', 'api/chores');
+	xhr.open('GET', 'api/chores', true);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
-			if (xhr.status === 200) {
+			if (xhr.status === 200 || xhr.status === 201) {
 				let chores = JSON.parse(xhr.responseText);
+				console.log(chores);
 				displayChoresList(chores);
 			}
 			else {
-				//TODO: show errors.
+				console.log(xhr.status);
 			}
 		}
 	};
-
 	xhr.send();
 }
 
 function displayChoresList(chores) {
+	let choreCount = 0;
 	let tbody = document.getElementById('choresTableRows');
-	tbody.textContent = '';
+	//	tbody.textContent = '';
+	while (tbody.firstElementChild) {
+		tbody.removeChild(tbody.firstElementChild);
+	}
 	for (let chore of chores) {
 		let trow = document.createElement('tr');
+		trow.id = chore.id;
 		trow.addEventListener('click', function(e) {
 			displayChore(chore);
 		});
@@ -47,8 +54,17 @@ function displayChoresList(chores) {
 		td = document.createElement('td');
 		td.textContent = '$' + chore.price;
 		trow.appendChild(td);
+		let deleteButton = document.createElement('input');
+		deleteButton.type = 'button';
+		deleteButton.addEventListener('click', deleteChore);
+		deleteButton.value = 'Delete';
+		td.appendChild(deleteButton);
+		trow.appendChild(td);
 		tbody.appendChild(trow);
+		choreCount += 1;
 	}
+	let choreCountSpan = document.getElementById('choreCountSpan');
+	choreCountSpan.textContent = choreCount;
 }
 
 function displayChore(chore) {
@@ -76,12 +92,13 @@ function updateChore(e) {
 
 function sendUpdatedChore(chore) {
 	let xhr = new XMLHttpRequest();
-	xhr.open('PUT', `api/chores/${chore.id}`);
+	xhr.open('PUT', `api/chores/${chore.id}`, true);
 	xhr.onreadystatechange = function() {
 		if (xhr.status === 4) {
 			if (xhr.status === 200 || xhr.status === 201) {
 				let chore = JSON.parse(xhr.responseTest);
 				console.log(chore);
+				loadChoresList();
 			}
 			else {
 				displayError('Error creating series: ' + xhr.status + " " + xhr.statusText);
@@ -90,11 +107,61 @@ function sendUpdatedChore(chore) {
 	};
 	xhr.setRequestHeader('Content-type', 'application/json');
 	xhr.send(JSON.stringify(chore));
-	
-	init();	
+
 
 }
 
+function addChore(e) {
+	e.preventDefault();
+	let form = document.newChoreForm;
+	let newChore = {
+		name: form.newChoreName.value,
+		price: form.newChorePrice.value
+	};
+	console.log(newChore);
+	sendNewChore(newChore);
+}
+
+function sendNewChore(chore) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', `api/chores/`, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.status === 4) {
+			if (xhr.status === 200 || xhr.status === 201) {
+				let chore = JSON.parse(xhr.responseTest);
+				console.log(chore);
+				loadChoresList();
+			}
+			else {
+				displayError('Error creating series: ' + xhr.status + " " + xhr.statusText);
+			}
+		}
+	};
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.send(JSON.stringify(chore));
+
+}
+
+function deleteChore(e) {
+	console.log('inside delete chore function');
+	let xhr = new XMLHttpRequest();
+	let id = e.target.parentElement.parentElement.id;
+	console.log('chore id: ' + id);
+	xhr.open('DELETE', `api/chores/${id}`, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.status === 4) {
+			if (xhr.status === 200 || xhr.status === 204) {
+				console.log('chore deleted');
+				loadChoresList();
+			}
+			else {
+				displayError('Error creating series: ' + xhr.status + " " + xhr.statusText);
+			}
+		}
+	}
+	xhr.send();
+
+}
 
 /*
 function displayChore(chore) {
