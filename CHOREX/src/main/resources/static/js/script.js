35bedaec0a65e80283fa70cfde6b1a89936d5082
Matt Elmore <84700909+dawabar@users.noreply.github.com>
2022-06-12 23:13:04 -0400

    I can display a single chore. I can update the chore, but the cached value remains in the page. Not sure how to fix that.

--- CHOREX/src/main/resources/static/js/script.js
+++ CHOREX/src/main/resources/static/js/script.js
@@ -1,12 +1,13 @@
 window.addEventListener('load', function(e) {
 	console.log('script.js loaded');
 	init();
-	e.preventDefault();
+	//	e.preventDefault();
 });
 
 function init() {
 	console.log('In init');
 	loadChoresList();
+	document.updateChoreForm.updateChoreButton.addEventListener('click', updateChore);
 }
 
 function loadChoresList() {
@@ -52,9 +53,82 @@ function displayChoresList(chores) {
 
 function displayChore(chore) {
 	console.log(chore.name);
+	let updateChoreId = document.getElementById('updateChoreId');
+	updateChoreId.value = chore.id;
+	updateChoreId.disabled = true;
+	let updateChoreName = document.getElementById('updateChoreName');
+	updateChoreName.value = chore.name;
+	let updateChorePrice = document.getElementById('updateChorePrice');
+	updateChorePrice.value = chore.price;
+}
+
+function updateChore(e) {
+	e.preventDefault();
+	let form = document.updateChoreForm;
+	let changedChore = {
+		id: form.updateChoreId.value,
+		name: form.updateChoreName.value,
+		price: form.updateChorePrice.value
+	};
+	console.log(changedChore);
+	sendUpdatedChore(changedChore);
+}
+
+function sendUpdatedChore(chore) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('PUT', `api/chores/${chore.id}`);
+	xhr.onreadystatechange = function() {
+		if (xhr.status === 4) {
+			if (xhr.status === 200 || xhr.status === 201) {
+				let chore = JSON.parse(xhr.responseTest);
+				console.log(chore);
+			}
+			else {
+				displayError('Error creating series: ' + xhr.status + " " + xhr.statusText);
+			}
+		}
+	};
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.send(JSON.stringify(chore));
+	
+	init();	
+
+}
+
+
+/*
+function displayChore(chore) {
+	console.log(chore.name);
 	let choreDiv = document.getElementById('chore');
+	let h3 = document.createElement('h3');
+	let br = document.createElement('br');
+	let td = document.createElement('td');
+	let table = document.createElement('table'); 
+	let thead = document.createElement('thead');
+	table.appendChild(thead);
+	let th = document.createElement('th');
+	for (let property in chore) {
+		if (chore.hasOwnProperty(property)) {
+			th.textContent = property;
+			thead.appendChild(th);
+		}
+	}
+	let tbody = document.createElement('tbody');
+	table.appendChild(tbody);
+	let tr = document.createElement('tr');
+	for (let property in chore) {
+		if (chore.hasOwnProperty(property)) {
+			for (val in property) {
+				td.textContent = property.val;
+				tr.appendChild(td);
+			}
+		}
+	}
+	choreDiv.appendChild('table');
+
 	choreDiv.textContent = chore.name;
 }
+
 
 function displayAddChoreForm(e) {
 	e.preventDefault();
@@ -67,6 +141,7 @@ function displayAddChoreForm(e) {
 		newChoreForm.removeClass('newChoreForm');
 	}
 }
+*/
 
 /* function showSeriesDetails(e) {
 	e.preventDefault();
